39ed88ed3d71e6bf52ff2b0119f932eebef559d9
Hendrik Peter van der Meulen <peter@picoreclame.nl>
2022-05-08 15:38:14 +0200

    Update to 2022

---
+++ precache-manifest.67c909b943575a6e4297ef510d2328ff.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a51d46927db06ef1e722b89db4020d0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "57ac43c910d41c16a8c4",
+    "url": "./static/css/main.1351c177.chunk.css"
+  },
+  {
+    "revision": "93a61ed7ab99f6320ec4",
+    "url": "./static/js/2.548b8052.chunk.js"
+  },
+  {
+    "revision": "0ed34690bd963db37b6985a388d78fc0",
+    "url": "./static/js/2.548b8052.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57ac43c910d41c16a8c4",
+    "url": "./static/js/main.df7c6120.chunk.js"
+  },
+  {
+    "revision": "ed313bbbd2181411661f",
+    "url": "./static/js/runtime-main.9478a5d6.js"
+  },
+  {
+    "revision": "fe0f65fdf163b744b7eb118bb6c99da1",
+    "url": "./static/media/background.fe0f65fd.jpg"
+  },
+  {
+    "revision": "8889009257da531a072c779cc3a6d1e9",
+    "url": "./static/media/bilda.88890092.png"
+  },
+  {
+    "revision": "0976f3f78a561aae6ee213f9dca5033c",
+    "url": "./static/media/boatkids.0976f3f7.jpg"
+  },
+  {
+    "revision": "8774674b42cbdc21778194540960605c",
+    "url": "./static/media/finalpic.8774674b.jpg"
+  },
+  {
+    "revision": "c9eebfe1c2b4a61c236060c9a63cf064",
+    "url": "./static/media/horsies.c9eebfe1.jpg"
+  },
+  {
+    "revision": "a78828fde30fc6c5f63c47a479d5590d",
+    "url": "./static/media/logo.a78828fd.png"
+  },
+  {
+    "revision": "5faa30482946f6a7b0ea9ad17f162187",
+    "url": "./static/media/roboto1.5faa3048.woff2"
+  },
+  {
+    "revision": "4b9fbc8a0001be0453e4bce7d4bc1649",
+    "url": "./static/media/roboto2.4b9fbc8a.woff2"
+  },
+  {
+    "revision": "2a2da422272c1e3c800bcc0c8fb4ae65",
+    "url": "./static/media/roboto3.2a2da422.woff2"
+  },
+  {
+    "revision": "29e17384db30ae7d29132f6ae08b5ecc",
+    "url": "./static/media/roboto4.29e17384.woff2"
+  },
+  {
+    "revision": "694af05c2fbef27a364c221536541df8",
+    "url": "./static/media/roboto5.694af05c.woff2"
+  },
+  {
+    "revision": "7d55b6166346d7d00b1ead589a7c48d4",
+    "url": "./static/media/roboto6.7d55b616.woff2"
+  },
+  {
+    "revision": "7a01d1a32f6b97a3b98c93ebf263f746",
+    "url": "./static/media/rules-aside.7a01d1a3.jpg"
+  },
+  {
+    "revision": "9b284bc1da50fa5fc03cdc952bc290cd",
+    "url": "./static/media/station.9b284bc1.jpg"
+  },
+  {
+    "revision": "7491707e3c9fdb382a06d74b456445b6",
+    "url": "./static/media/theme-picture.7491707e.jpg"
+  },
+  {
+    "revision": "626e95267fa7c66789108a2baeac5726",
+    "url": "./static/media/ultra.626e9526.woff2"
+  }
+]);
